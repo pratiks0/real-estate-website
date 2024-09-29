23200644d07f8c5c758040165a16e097d4c2f265
Pratik Singh <pratik00888@gmail.com>
2024-09-29 20:38:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-export default function App() {
+import React from 'react'
+import { DarkModeProvider } from './components/DarkModeContext'
+import About from './section/About'
+import Clients from './section/Clients'
+import Contact from './section/Contact'
+import Hero from './section/Hero'
+import PopularAreas from './section/PopularAreas'
+import Services from './section/Services'
+import Properties from './section/Properties'
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <DarkModeProvider>
+        <Header/>
+        <Hero/>
+        <About/>
+        <PopularAreas/>
+        <Properties/>
+        <Services/>
+        <Clients/>
+        <Contact/>
+        <Footer/>
+      </DarkModeProvider>
+    </>
   )
 }
+
+export default App
---
+++ src/section/PopularAreas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopularAreas = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PopularAreas
