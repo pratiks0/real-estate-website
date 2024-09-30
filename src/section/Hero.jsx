7df8470782ca2a98b80d6a2ea47e0e44e8a3278d
Pratik Singh <pratik00888@gmail.com>
2024-09-30 19:16:17 +0530

    section coding

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDarkMode } from '../components/DarkModeContext'
 import heroimg from '../../assets/images/hero1.webp'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const Hero = () => {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      offset: 200,
+      easing: 'ease-in',
+      delay: 100,
+    })  
+  }, [])
+
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <div>
       
