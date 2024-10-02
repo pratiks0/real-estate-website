3ed2eec97b14b2188895bc523fc88295a27c3161
Pratik Singh <pratik00888@gmail.com>
2024-10-02 23:04:06 +0530

    final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,6 @@ import {
 } from "react-icons/fa";
 import { Link } from "react-scroll";
 import { IoMdMail } from "react-icons/io";
-import { Fa0 } from "react-icons/fa6";
 
 const Footer = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
@@ -25,10 +24,10 @@ const Footer = () => {
         className={`${
           darkMode ? "dark bg-black" : "light bg-gray-800"
         } w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`}
-        style={{fontFamily: '"Raleway", sans-serif'}}
+        style={{ fontFamily: '"Raleway", sans-serif' }}
       >
         <div className="flex flex-col justify-center items-start gap-5">
-          <h1 className="text-white text-2xl font-semi">About Us</h1>
+          <h1 className="text-white text-2xl font-semibold">About Us</h1>
           <p className="text-slate-200 text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
             reiciendis quam magnam! Vitae ipsam fugiat aspernatur vel!
@@ -51,7 +50,7 @@ const Footer = () => {
             </div>
           </div>
           <h1 className="text-white mt-8">
-            Coptright Real Estate, All Rights Reserved
+            Copyright Real Estate, All Rights Reserved
           </h1>
         </div>
 
@@ -76,19 +75,22 @@ const Footer = () => {
         </div>
       </footer>
 
-      <div
-        id="icon-box"
-        className="bg-red-600 p-4 rounded-full hover:bg-black cursor-pointer fixed lg:bottom-12 bottom-6 right-6"
-      >
-        <Link to="hero" spy={true} offset={-100} smooth={true}>
-          <FaArrowUp className="text-white size-5" />
-        </Link>
-      </div>
-
-      <div>
-        <button onClick={toggleDarkMode} className="flex items-center p-4 rounded-full bg-orange-500 fixed lg:top-52 right-6 top-6">
-          {darkMode ? <FaMoon size={25} className="text-black"/> : <FaSun size={25} className='text-black'/>}
+      <div className="fixed bottom-16 right-6 z-50"> {/* Positioning above the icon-box */}
+        <button
+          onClick={toggleDarkMode}
+          className="flex items-center p-4 rounded-full bg-orange-500 mb-2" // Added margin-bottom for spacing
+        >
+          {darkMode ? <FaMoon size={25} className="text-black" /> : <FaSun size={25} className="text-black" />}
         </button>
+        
+        <div
+          id="icon-box"
+          className="bg-red-600 p-4 rounded-full hover:bg-black cursor-pointer"
+        >
+          <Link to="hero" spy={true} offset={-100} smooth={true}>
+            <FaArrowUp className="text-white size-5" />
+          </Link>
+        </div>
       </div>
     </>
   );
